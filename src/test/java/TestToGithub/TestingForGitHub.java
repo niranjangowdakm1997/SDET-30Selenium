af5ce99babf07e581a1c50beecf9fa5c1d29d105
User <User@DESKTOP-141UIPT>
2022-03-10 15:16:27 +0530

    1.First Person-Niranjan

---
+++ src/test/java/TestToGithub/TestingForGitHub.java
@@ -0,0 +1,14 @@
+package TestToGithub;
+
+import org.testng.annotations.Test;
+
+public class TestingForGitHub {
+	
+	@Test
+	
+	public void connectToGitHub()
+	{
+		System.out.println("Hi am Available");
+	}
+
+}
