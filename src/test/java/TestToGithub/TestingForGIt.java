620846d88e7af6ddc1052e60982ac2a6d5a4deeb
User <User@DESKTOP-141UIPT>
2022-03-10 15:45:36 +0530

    2.Second Person-Niranjan

---
+++ src/test/java/TestToGithub/TestingForGIt.java
@@ -0,0 +1,14 @@
+package TestToGithub;
+
+import org.testng.annotations.Test;
+
+public class TestingForGIt {
+
+	
+	@Test
+	
+	public void connectToGitHub1()
+	{
+		System.out.println("Hi am Available second time");
+	}
+}
